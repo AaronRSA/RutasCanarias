1cfdf0e26ec0f7a135f5c1bdd908d4ccd5a1fb54
Rubén Díaz Martínez <rubendm92@gmail.com>
2014-02-25 14:16:59 +0000

    Added structure

---
+++ control/FrontCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+abstract class FrontCommand {
+    
+    protected function forward() {
+        extract($_POST);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url());
+        curl_setopt($ch, CURLOPT_POST, count($this->fields()));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->fieldsString($this->fields()));
+        curl_exec($ch);
+        curl_close($ch);
+    }
+    
+    protected abstract function url();
+    
+    protected abstract function fields();
+    
+    private function fieldsString($fields) {
+        $fields_string = "";
+        foreach ($fields as $key => $value) {
+            $fields_string .= $key . '=' . $value . '&';
+        }
+        rtrim($fields_string, '&');
+        return $fields_string;
+    }
+}
